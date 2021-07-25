be83be582c4b17c75917ad5d019c5eb5c6712a3f
joaolavelino <joaolavelino@gmail.com>
2021-07-25 11:45:05 -0300

    commit inicial - sanar dúvidas em relação ao Material UI com instrutor da Alura

---
+++ src/components/Formulario/FormularioCadastro.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Button from "@material-ui/core/Button"
+
+function FormularioCadastro () {
+
+    return(
+        <form>
+            <label>Nome</label>
+            <input type="text" />
+            
+            <label>Sobrenome</label>
+            <input type="text" />
+
+            <label>CPF</label>
+            <input type="text" />
+
+            <label>Promoções</label>
+            <input type="checkbox" name="" id="" />
+
+            <label>Novidades</label>
+            <input type="checkbox" name="" id="" />
+
+            <Button type="submit" variant="contained" color="primary">Cadastrar</Button>
+
+        </form>
+    )
+
+};
+
+export default FormularioCadastro
+
